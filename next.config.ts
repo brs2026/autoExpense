7c3bfbfd3781db21b6c6a0d274fdd223f7f709c2
Rakib Uddin <96006245+rakib08@users.noreply.github.com>
2026-05-27 16:33:45 +0600

    PWA fixed

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,13 @@ import withPWA from "next-pwa";
 
 const nextConfig = {
   reactStrictMode: true,
+
+  turbopack: {},
 };
 
 export default withPWA({
   dest: "public",
+  register: true,
+  skipWaiting: true,
   disable: process.env.NODE_ENV === "development",
 })(nextConfig);
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,11 +4,11 @@ import { LanguageProvider } from "@/context/language-context";
 
 import "./globals.css";
 
-export const metadata: Metadata = {
+export const metadata = {
   title: "AutoExpense",
-  description: "Vehicle expense management system",
-  manifest: "/manifest.json",
-  themeColor: "#000000",
+  description: "Expense Manager",
+
+  metadataBase: new URL("https://auto-expense-sigma.vercel.app"),
 };
 
 export default function RootLayout({
